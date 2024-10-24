160e43abf022598f9c59c0ae5cdf8de854269e09
Guo Xinyu <xinyu.guo655@gmail.com>
2024-10-25 04:49:32 +0800

    Problem104

---
+++ MaxDepthBinaryTree.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+
+    def dfs(self, node, depth):
+        
+        # you find a leaf
+        if node.left == None and node.right == None:
+            return depth
+        
+        # what if this node is not a leaf
+        # three situations:
+        # left child only
+        if node.right == None:
+            return self.dfs(node.left, depth+1)
+        elif node.left == None:
+            return self.dfs(node.right, depth+1)
+        else:
+            return max( self.dfs(node.left, depth+1), self.dfs(node.right, depth+1) )
+        
+
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        # maximum depth:  the number of nodes along the longest path 
+        # from the root node down to the farthest leaf node.
+
+        if root == None: 
+            return 0
+        else:
+            return self.dfs(root, 1)
+
+
+        
