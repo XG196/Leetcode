eeff2e0a9a66ef36df93f9dbc7177ba03990fe0d
XG196 <xinyu.guo655@gmail.com>
2024-10-29 05:31:06 +0800

    Problem784

---
+++ letterCasePermutation.py
@@ -0,0 +1,40 @@
+class Solution:
+    
+    
+    def getPmt(self, s, i, cons, list_of_pmt):
+
+        if i == len(s):
+            list_of_pmt.append(cons)  
+            return
+        
+        if s[i].isalpha():
+            new_str = cons + s[i].lower()
+            self.getPmt(s, i+1, new_str, list_of_pmt)
+
+            new_str = cons + s[i].upper()
+            self.getPmt(s, i+1, new_str, list_of_pmt)
+
+        else:
+            new_str = cons + s[i]
+            self.getPmt(s, i+1, new_str, list_of_pmt)
+
+        return
+
+
+    def letterCasePermutation(self, s: str):
+
+        # return a list of all possible strings
+        list_of_pmt = []
+        self.getPmt(s.lower(), 0, "", list_of_pmt)
+
+        return list_of_pmt
+
+
+def main():
+    sol = Solution()
+    sol.letterCasePermutation("A1B3")
+
+
+main()
+
+
