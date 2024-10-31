4f5eb847e1f6919f5b2e544eea3f715d915f8d3e
XG196 <xinyu.guo655@gmail.com>
2024-10-31 09:52:46 +0800

    Problem90

--- Subsets2.py
+++ Subsets2.py
@@ -1,8 +1,28 @@
 class Solution:
 
-    
+    def backtrack(self, start, nums, tmp, stoplevel):
+
+        if len(tmp) == stoplevel:
+            self.output.append(tmp[:])
+
+        # do backtrack here
+        for i in range(start, len(nums)):
+
+            if i > 0 and nums[i] == nums[i-1] and i != start:
+                continue
+
+            tmp.append(nums[i])
+            self.backtrack(i+1, nums, tmp, stoplevel)
+            tmp.pop()
+
     def subsetsWithDup(self, nums: list[int]):
 
-
-        pass
+        nums.sort()
+        self.output = []
+        self.output.append([])
+        self.n = len(nums)
+        for stoplevel in range(self.n):
+            self.backtrack(0, nums, [], stoplevel+1)
+            
+        return self.output
         
